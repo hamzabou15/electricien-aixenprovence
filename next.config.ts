f276d1c16d537bcd2943adceea3f3061177f28f6
hamza bouyoussef <hamza8pro@gmail.com>
2025-07-30 18:05:33 +0100

    next-congig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
+  images: {
+    domains: ['electricientoulon-expert.fr'],
+  },
 };
+module.exports = nextConfig;
 
 module.exports = nextConfig;
